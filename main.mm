9ab540302e53f58a3a9ab6a9a3b34e87ea610eaf
Cykey <cykey@live.com>
2012-07-23 20:17:28 -0500

    Stuff

--- main.mm
+++ main.mm
@@ -1,6 +1,17 @@
 #import <AudioToolbox/AudioToolbox.h>
 
+static void usage()
+{
+    fprintf(stdout,"vibrate: usage: vibrate [seconds]\n");
+}
+
 int main(int argc, char **argv, char **envp) {
+    
+    if (argc < 2) {
+        usage();
+        return -1;
+    }
+    
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	NSDate *now = [NSDate date];
 	char *s = argv[1];
